7bbc562f97618584cc92f504d0222b0e909bd33d
Ll <dididy@knou.ac.kr>
2018-08-13 05:13:58 -0700

    Create app_2.py

---
+++ app_2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+
+delay = []
+driver = webdriver.Firefox()
+driver.implicitly_wait(3)
+driver.get('http://its.go.kr/traffic/condition.do')
+sleep(5)
+
+h = driver.page_source
+
+root = BeautifulSoup(h,'html.parser')
+a = root.find_all('ul', {'id':'congestedAreaList'})
+delay = a.splitlines('\\n')
+#print(a)
+for i in range(len(a)):
+    delay.append(a[i].get_text())
+print(delay)
